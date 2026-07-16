c3b63ca6cdc325315eab8e284d8bec344a2e8a4f
revanstore235 <revindradra@gmail.com>
2026-07-16 11:00:48 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,12 +1,13 @@
 const setting = {
     botName: 'KickBot',
-    version: '4.0.0',
+    version: '4.1.0',
     prefix: '!',
-    ownerNumber: '6281284406155@s.whatsapp.net', // fixed: sebelumnya salah ketik 156
-    botNumber: '6283180391763', // nomor yang dipakai untuk pairing code (nomor bot)
+    ownerNumber: '6281284406155@s.whatsapp.net',  // FIXED: nomor owner yg benar
+    botNumber: '6283180391763',                    // Nomor WhatsApp yang akan dijadikan bot (untuk pairing)
     ownerName: 'Developer',
     sessionPath: './session',
     reconnectDelay: 5000,
+    connectTimeoutMs: 20000,
     cooldowns: {
         hidetag: 10,
         kick: 5,
